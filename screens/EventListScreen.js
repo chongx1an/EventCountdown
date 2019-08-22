9dd335dad00fc262885addb223d4dad95c00ba97
kkloh98 <kkloh9898@gmail.com>
2019-08-22 22:37:45 +0800

    Added firestore fetch to event list

--- screens/EventListScreen.js
+++ screens/EventListScreen.js
@@ -5,10 +5,75 @@ import {
   Text,
   ScrollView,
   TouchableOpacity,
+  FlatList
 } from 'react-native';
 import Event from './UI/Event';
+import firebase from 'react-native-firebase';
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 class EventListScreen extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      events: [],
+      loading: false,
+    };
+
+    this.ref = firebase.firestore().collection('events');
+  }
+
+  componentDidMount() {
+    this.unsubsribe = this.ref.onSnapshot((querySnapshot) => {
+
+      const eventsData = [];
+      
+      querySnapshot.forEach((doc) => {
+        eventsData.push({
+          title: doc.data().title,
+          date: doc.data().date,
+          participants: doc.data().participants
+        });
+      });
+
+      this.setState({
+        events: eventsData.sort((a, b) => {
+          return (a.date > b.date);
+        }),
+        loading: false
+      })
+    });
+  }
+
+  timeDifference(date1, date2) {
+      var difference = date1.getTime() - date2.getTime();
+
+      var daysDifference = Math.floor(difference/1000/60/60/24);
+      difference -= daysDifference*1000*60*60*24
+
+      var hoursDifference = Math.floor(difference/1000/60/60);
+      difference -= hoursDifference*1000*60*60
+
+      var minutesDifference = Math.floor(difference/1000/60);
+      difference -= minutesDifference*1000*60
+
+      var secondsDifference = Math.floor(difference/1000);
+
+    //return 'difference = ' + daysDifference + ' day/s ' + hoursDifference + ' hour/s ' + minutesDifference + ' minute/s ' + secondsDifference + ' second/s ';
+    return daysDifference; 
+  }
+
+  toDate(timestamp) {
+    var toDate = new Date(timestamp * 1000);
+    var date = toDate.getDate();
+    var month = toDate.getMonth() + 1;
+    var year = toDate.getFullYear();
+
+    return date + ' ' + months[month] + ' ' + year;
+  }
+
   render() {
     return (
         <View style={styles.root}>
@@ -16,16 +81,16 @@ class EventListScreen extends Component{
             <Text style={styles.text}>List{"\n"}of Events</Text>
 
             <View style={{height: 300}}>
-                <ScrollView>
-                <Event activity="Hang Out" date="25 July 2019" days="3"/>
-                <Event activity="Hang Out" date="25 July 2019" days="3"/>
-                <Event activity="Hang Out" date="25 July 2019" days="3"/>
-                <Event activity="Hang Out" date="25 July 2019" days="3"/>
-                <Event activity="Hang Out" date="25 July 2019" days="3"/>
-                <Event activity="Hang Out" date="25 July 2019" days="3"/>
-                <Event activity="Hang Out" date="25 July 2019" days="3"/>
-                <Event activity="Hang Out" date="25 July 2019" days="3"/>
-                </ScrollView>
+              <FlatList
+                data={this.state.events}
+                renderItem={({item, index}) =>
+                  <Event activity={item.title}
+                  date={this.toDate(item.date)}
+                  days={this.timeDifference(new Date(item.date * 1000), new Date())}/>
+                }
+                keyExtractor={(item, index) => item.title}
+              >
+              </FlatList>
             </View>
 
             <View style={styles.buttons}>
